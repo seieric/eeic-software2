f4d0b80d08c484b0eb52c1298e68d59340dbc1b5
seieric <30688855+seieric@users.noreply.github.com>
2024-12-15 21:04:24 +0900

    update: 不要なプロトタイプ宣言を削除

--- week2/my_darts1.h
+++ week2/my_darts1.h
@@ -21,37 +21,28 @@ typedef struct board {
 // 初期化して円を表示する
 //  * なにもない場所は ' 'を代入します
 // 円の縁に相当する部分に'+'を代入します
-void init_board(Board *b);
 void my_init_board(Board *b);
 
 // 盤面を表示
-void print_board(Board *b);
 void my_print_board(Board *b);
 
 // ボード構造体内の配列の上限を取得
-size_t get_board_height(Board *b);
-size_t get_board_width(Board *b);
 size_t my_get_board_height(Board *b);
 size_t my_get_board_width(Board *b);
 
 // i回目 (1-3) が盤面内なら数字でプロット
-void plot_throw(Board *b, Point p, int i);
 void my_plot_throw(Board *b, Point p, int i);
 
 // 座標が描画領域ならtrueを返す
-bool is_in_board(Board *b, Point p);
 bool my_is_in_board(Board *b, Point p);
 
 // 座標が有効（得点圏内）ならtrueを返す
-bool is_valid_point(Board *b, Point p);
 bool my_is_valid_point(Board *b, Point p);
 
 // 分散が等方向一定の正規分布の乱数を生成する
-Point iso_gauss_rand(Point mu, double stddev);
 Point my_iso_gauss_rand(Point mu, double stddev);
 
 // 座標を (? ?) で表示（改行なし）
-void print_point(Point p);
 void my_print_point(Point p);
 
 int my_calculate_score(Board *b, Point p);
