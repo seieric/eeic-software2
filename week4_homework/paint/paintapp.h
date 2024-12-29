56c128c75cb5db30e2dfbfbd7657e380cd1dfb31
seieric <30688855+seieric@users.noreply.github.com>
2024-12-29 10:34:33 +0900

    update: 宣言を分離

---
+++ week4_homework/paint/paintapp.h
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// Structure for canvas
+typedef struct
+{
+    int width;
+    int height;
+    char **canvas;
+    char pen;
+} Canvas;
+
+// Command 構造体と History構造体
+// [*]
+typedef struct command Command;
+struct command{
+    char *str;
+    size_t bufsize;
+    Command *next;
+};
+
+typedef struct
+{
+    Command *begin;
+    size_t bufsize; // [*] : この方が効率的ですね。一部の方から指摘ありました。
+} History;
+
+// functions for Canvas type
+Canvas *init_canvas(int width, int height, char pen);
+void reset_canvas(Canvas *c);
+void print_canvas(Canvas *c);
+void free_canvas(Canvas *c);
+
+// display functions
+void rewind_screen(unsigned int line); 
+void clear_command(void);
+void clear_screen(void);
+
+// enum for interpret_command results
+typedef enum res{ EXIT, LINE, UNDO, SAVE, UNKNOWN, ERRNONINT, ERRLACKARGS, NOCOMMAND} Result;
+// Result 型に応じて出力するメッセージを返す
+char *strresult(Result res);
+
+int max(const int a, const int b);
+void draw_line(Canvas *c, const int x0, const int y0, const int x1, const int y1);
+Result interpret_command(const char *command, History *his, Canvas *c);
+void save_history(const char *filename, History *his);
+
+// [*] list.c のpush_backと同じ
+Command *push_command(History *his, const char *str);
