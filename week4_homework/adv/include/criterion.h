cd6f7e7641eccf16a6040fae78a6c8c122de8a75
seieric <30688855+seieric@users.noreply.github.com>
2025-01-02 23:31:54 +0900

    add: cross entropyの微分

--- week4_homework/adv/include/criterion.h
+++ week4_homework/adv/include/criterion.h
@@ -2,3 +2,4 @@
 #pragma once
 
 double cross_entropy_loss(double pred, double ans);
+double dcross_entropy_loss_dpred(double pred, double ans);
--- week4_homework/adv/src/criterion.c
+++ week4_homework/adv/src/criterion.c
@@ -1,10 +1,14 @@
 #include "criterion.h"
 
-#include <math.h>
 #include <assert.h>
+#include <math.h>
 
 double cross_entropy_loss(double pred, double ans) {
     assert(0 < pred && pred < 1);
     assert(0 <= ans && ans <= 1);
     return -ans * log2(pred) - (1 - ans) * log2(1 - pred);
+}
+
+double dcross_entropy_loss_dpred(double pred, double ans) {
+    return -ans / pred + (1 - ans) / (1 - pred);  // 定数は無視
 }
