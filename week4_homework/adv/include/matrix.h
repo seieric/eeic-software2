f15c2462ea3d13ccecd07dc3b5b0dfd276db9b18
seieric <30688855+seieric@users.noreply.github.com>
2025-01-02 18:35:47 +0900

    add: He初期化関数

--- week4_homework/adv/include/matrix.h
+++ week4_homework/adv/include/matrix.h
@@ -1,6 +1,9 @@
 typedef struct mat Mat;
 
 Mat *mat_mul(Mat *a, Mat *b);
-Mat *mat_create(const int height, const int width, double array[]);
+Mat *mat_create(const int height, const int width);
 void mat_destroy(Mat *a);
 void mat_print(Mat *a);
+
+// 初期化
+void mat_he_init(Mat *a);
--- week4_homework/adv/src/matrix.c
+++ week4_homework/adv/src/matrix.c
@@ -1,8 +1,11 @@
 #include "matrix.h"
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <math.h>
+
+#define PI 3.14159265358979323846
 
 struct mat {
     int height;
@@ -14,21 +17,23 @@ Mat *mat_mul(Mat *a, Mat *b) {
     assert(a->width == b->height);
     const int h = a->height;
     const int w = b->width;
-    double *array = (double *)malloc(h * w * sizeof(double));
+
+    Mat *mat = mat_create(h, w);
     for (int i = 0; i < h; ++i) {
         for (int j = 0; j < w; ++j) {
             double sum = 0;
             for (int k = 0; k < a->width; ++k) {
                 sum += a->array[i * a->width + k] * b->array[j + k * w];
             }
-            array[i * w + j] = sum;
+            mat->array[i * w + j] = sum;
         }
     }
-    return mat_create(h, w, array);
+    return mat;
 }
 
-Mat *mat_create(const int height, const int width, double *array) {
+Mat *mat_create(const int height, const int width) {
     Mat *mat = (Mat *)malloc(sizeof(Mat));
+    double *array = (double *)malloc(height * width * sizeof(double));
     *mat = (Mat){.height = height, .width = width, .array = array};
     return mat;
 }
@@ -45,5 +50,17 @@ void mat_print(Mat *a) {
             printf("%lf ", a->array[i * a->width + j]);
         }
         printf("]\n");
+    }
+}
+
+void mat_he_init(Mat *a) {
+    const int n = a->height * a->width;
+    const double stddev = sqrt(2.0 / a->width);  // 行列の列数が入力次元数
+
+    for (int i = 0; i < n; ++i) {
+        // ボックス・ミュラー法（第2回）
+        double u1 = rand() * 1.0 / RAND_MAX;
+        double u2 = rand() * 1.0 / RAND_MAX;
+        a->array[i] = sqrt(-2 * log(u1)) * cos(2 * PI * u2) * stddev;
     }
 }
