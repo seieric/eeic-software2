16fd9fc6473d701e85616f1c1350013291d6e1a6
seieric <30688855+seieric@users.noreply.github.com>
2025-01-03 22:43:29 +0900

    update: logの発散を回避

--- week4_homework/adv/src/criterion.c
+++ week4_homework/adv/src/criterion.c
@@ -3,9 +3,14 @@
 #include <assert.h>
 #include <math.h>
 
+#define EPS 1e-7
+
 double cross_entropy_loss(double pred, double ans) {
-    assert(0 < pred && pred < 1);
+    assert(0 <= pred && pred <= 1);
     assert(0 <= ans && ans <= 1);
+    // 0や1ではlogが計算できないのでクリップして回避する
+    if (pred < EPS) pred = EPS;
+    if (pred > 1 - EPS) pred = 1 - EPS;
     return -ans * log2(pred) - (1 - ans) * log2(1 - pred);
 }
 
