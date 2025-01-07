87ad7ea01c30e123dad150e36d1b86454f52709f
seieric <30688855+seieric@users.noreply.github.com>
2025-01-07 18:55:39 +0900

    update: 重量制限違反を探索しない

--- week5_homework/knapsack1/src/search.c
+++ week5_homework/knapsack1/src/search.c
@@ -1,9 +1,9 @@
 #include "search.h"
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #include "item.h"
 
@@ -18,16 +18,18 @@ double solve(const Itemset *list, double capacity) {
 }
 
 // 再帰的な探索関数
-double search(int index, const Itemset *list, double capacity, unsigned char *flags, double sum_v, double sum_w) {
+double search(int index, const Itemset *list, double capacity,
+              unsigned char *flags, double sum_v, double sum_w) {
     size_t max_index = get_nitem(list);
     assert(index >= 0 && sum_v >= 0 && sum_w >= 0);
 
     // 必ず再帰の停止条件を明記する (最初が望ましい)
     if (index == max_index) {
         const char *format_ok = ", total_value = %5.1f, total_weight = %5.1f\n";
-        const char *format_ng = ", total_value = %5.1f, total_weight = %5.1f NG\n";
+        const char *format_ng =
+            ", total_value = %5.1f, total_weight = %5.1f NG\n";
 
-        bool is_not_exceed = (sum_w <= capacity); // 重さ評価のtrue/false
+        bool is_not_exceed = (sum_w <= capacity);  // 重さ評価のtrue/false
         for (int i = 0; i < max_index; i++) {
             printf("%d", flags[i]);
         }
@@ -47,7 +49,9 @@ double search(int index, const Itemset *list, double capacity, unsigned char *fl
     Item *item = get_item(list, index);
     sum_v += get_itemvalue(item);
     sum_w += get_itemweight(item);
-    const double v1 = search(index + 1, list, capacity, flags, sum_v, sum_w);
+    const bool is_exceed = (sum_w > capacity);
+    const double v1 =
+        is_exceed ? 0 : search(index + 1, list, capacity, flags, sum_v, sum_w);
 
     // 使った場合の結果と使わなかった場合の結果を比較して返す
     return (v0 > v1) ? v0 : v1;
