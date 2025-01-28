14af0a631c82efb77dc74eadac617f66a710bddf
seieric <30688855+seieric@users.noreply.github.com>
2025-01-28 17:52:29 +0900

    add: implementation of traverse_tree

--- week6_homework/huffman0/src/encode.c
+++ week6_homework/huffman0/src/encode.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 #include "encode.h"
 
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 // 構造体定義
-struct node{
+struct node {
     int symbol;
     int count;
     Node *left;
@@ -33,10 +34,8 @@ static Node *pop_min(int *n, Node *nodep[]);
 // ハフマン木を構成する関数
 static Node *build_tree(void);
 
-
 // 以下 static関数の実装
-static void count_symbols(const char *filename)
-{
+static void count_symbols(const char *filename) {
     FILE *fp = fopen(filename, "rb");
     if (fp == NULL) {
         fprintf(stderr, "error: cannot open %s\n", filename);
@@ -51,20 +50,18 @@ static void count_symbols(const char *filename)
 
     fclose(fp);
 }
-static void reset_count(void)
-{
-    for (int i = 0 ; i < NSYMBOLS ; i++) symbol_count[i] = 0;
+static void reset_count(void) {
+    for (int i = 0; i < NSYMBOLS; i++) symbol_count[i] = 0;
 }
 
-static Node *create_node(int symbol, int count, Node *left, Node *right)
-{
+static Node *create_node(int symbol, int count, Node *left, Node *right) {
     Node *ret = (Node *)malloc(sizeof(Node));
-    *ret = (Node){ .symbol = symbol, .count = count, .left = left, .right = right};
+    *ret =
+        (Node){.symbol = symbol, .count = count, .left = left, .right = right};
     return ret;
 }
 
-static Node *pop_min(int *n, Node *nodep[])
-{
+static Node *pop_min(int *n, Node *nodep[]) {
     // Find the node with the smallest count
     // カウントが最小のノードを見つけてくる
     int argmin = 0;
@@ -73,9 +70,9 @@ static Node *pop_min(int *n, Node *nodep[])
             argmin = i;
         }
     }
-    
+
     Node *node_min = nodep[argmin];
-    
+
     // Remove the node pointer from nodep[]
     // 見つかったノード以降の配列を前につめていく
     for (int i = argmin; i < (*n) - 1; i++) {
@@ -83,26 +80,25 @@ static Node *pop_min(int *n, Node *nodep[])
     }
     // 合計ノード数を一つ減らす
     (*n)--;
-    
+
     return node_min;
 }
 
-static Node *build_tree(void)
-{
+static Node *build_tree(void) {
     int n = 0;
     Node *nodep[NSYMBOLS];
-    
+
     for (int i = 0; i < NSYMBOLS; i++) {
         // カウントの存在しなかったシンボルには何もしない
         if (symbol_count[i] == 0) continue;
         nodep[n++] = create_node(i, symbol_count[i], NULL, NULL);
     }
 
-    const int dummy = -1; // ダミー用のsymbol を用意しておく
+    const int dummy = -1;  // ダミー用のsymbol を用意しておく
     while (n >= 2) {
         Node *node1 = pop_min(&n, nodep);
         Node *node2 = pop_min(&n, nodep);
-        
+
         // Create a new node
         // 選ばれた2つのノードを元に統合ノードを新規作成
         int dummy_count = node1->count + node2->count;
@@ -110,31 +106,80 @@ static Node *build_tree(void)
     }
 
     // なぜ以下のコードで木を返したことになるか少し考えてみよう
-    return (n==0)?NULL:nodep[0];
+    return (n == 0) ? NULL : nodep[0];
 }
-
-
 
 // Perform depth-first traversal of the tree
 // 深さ優先で木を走査する
-// 現状は何もしていない（再帰してたどっているだけ）
-void traverse_tree(const int depth, const Node *np)
-{			  
-    if (np == NULL || np->left == NULL) return;
-    
-    traverse_tree(depth + 1, np->left);
-    traverse_tree(depth + 1, np->right);
+void traverse_tree(const int depth, const Node *np) {
+    // ハフマン符号
+    // 右からdepthビット分が実際の符号（右から左に読む）
+    static unsigned int huffman = 0;
+    if (np == NULL) return;
+
+    if (depth == 0) {
+        printf(".");
+    }
+
+    // 自身の情報を表示
+    printf("[");
+    if (np->symbol == -1) {
+        printf("-1");
+    } else if (np->symbol == '\n') {
+        printf("\\n");
+    } else {
+        printf("%c", np->symbol);
+    }
+    printf("] ");
+    for (int i = 0; i < depth; ++i) {
+        if (huffman & 1 << i) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+    }
+    printf(" (cnt. %d)\n", np->count);
+
+    // 左の子
+    if (np->left) {
+        // 右からdepthビット目に0を立てる
+        huffman &= ~(1 << depth);
+        for (int i = 0; i < depth; ++i) {
+            if (huffman & 1 << i) {
+                printf(" ");
+            } else {
+                printf("│");
+            }
+            printf("   ");
+        }
+        printf("├──");
+        traverse_tree(depth + 1, np->left);
+    }
+    // 右の子
+    if (np->right) {
+        // 右からdepthビット目に1を立てる
+        huffman |= 1 << depth;
+        for (int i = 0; i < depth; ++i) {
+            if (huffman & 1 << i) {
+                printf(" ");
+            } else {
+                printf("│");
+            }
+            printf("   ");
+        }
+        printf("└──");
+        traverse_tree(depth + 1, np->right);
+    }
 }
 
 // この関数は外部 (main) で使用される (staticがついていない)
-Node *encode(const char *filename)
-{
+Node *encode(const char *filename) {
     reset_count();
     count_symbols(filename);
     Node *root = build_tree();
-    
-    if (root == NULL){
-        fprintf(stderr,"A tree has not been constructed.\n");
+
+    if (root == NULL) {
+        fprintf(stderr, "A tree has not been constructed.\n");
     }
 
     return root;
